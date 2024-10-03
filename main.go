a01881b4a92d3379174140a5e831cf982323a5ac
nikhuge <nikhuge@gmail.com>
2024-10-03 20:53:05 +0530

    done

---
+++ main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"time"
+
+	"github.com/coreos/go-systemd/v22/daemon"
+	"github.com/getsentry/sentry-go"
+	"github.com/godbus/dbus/v5"
+	"github.com/godbus/dbus/v5/introspect"
+	"github.com/godbus/dbus/v5/prop"
+
+	"github.com/Vioneta/os-agent/apparmor"
+	"github.com/Vioneta/os-agent/boards"
+	"github.com/Vioneta/os-agent/cgroup"
+	"github.com/Vioneta/os-agent/datadisk"
+	"github.com/Vioneta/os-agent/system"
+	logging "github.com/Vioneta/os-agent/utils/log"
+)
+
+const (
+	busName    = "io.hass.os"
+	objectPath = "/io/hass/os"
+	sentryDsn  = "https://c74e811a96e4413a95caaaa5ae05f851@o427061.ingest.sentry.io/5710878"
+)
+
+var (
+	// version and baord are set at link time via -X flag
+	version       string = "dev"
+	board         string = "unknown"
+	enableCapture bool   = false
+)
+
+func main() {
+	logging.Info.Printf("Start OS-Agent %s", version)
+
+	// Sentry
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn:        sentryDsn,
+		Release:    version,
+		BeforeSend: filterSentry,
+	})
+	if err != nil {
+		logging.Critical.Fatalf("Sentry init: %s", err)
+	}
+
+	defer sentry.Flush(2 * time.Second)
+	defer sentry.Recover()
+
+	// Connect DBus
+	conn, err := dbus.SystemBus()
+	if err != nil {
+		logging.Critical.Fatalf("DBus connection: %s", err)
+	}
+
+	// Init Dbus io.hass.os
+	reply, err := conn.RequestName(busName, dbus.NameFlagDoNotQueue)
+	if err != nil {
+		logging.Critical.Panic(err)
+	}
+	if reply != dbus.RequestNameReplyPrimaryOwner {
+		logging.Critical.Fatalf("name already taken")
+	}
+
+	// Set base Property / functionality
+	InitializeDBus(conn)
+
+	logging.Info.Printf("Listening on service %s ...", busName)
+	datadisk.InitializeDBus(conn)
+	system.InitializeDBus(conn)
+	apparmor.InitializeDBus(conn)
+	cgroup.InitializeDBus(conn)
+	boards.InitializeDBus(conn, board)
+
+	_, err = daemon.SdNotify(false, daemon.SdNotifyReady)
+	if err != nil {
+		logging.Critical.Panic(err)
+	}
+	select {}
+}
+
+func InitializeDBus(conn *dbus.Conn) {
+	propsSpec := map[string]map[string]*prop.Prop{
+		busName: {
+			"Version": {
+				Value:    version,
+				Writable: false,
+				Emit:     prop.EmitInvalidates,
+				Callback: nil,
+			},
+			"Diagnostics": {
+				Value:    enableCapture,
+				Writable: true,
+				Emit:     prop.EmitTrue,
+				Callback: func(c *prop.Change) *dbus.Error {
+					logging.Info.Printf("Diagnostics is now %t", c.Value)
+					enableCapture = c.Value.(bool)
+					return nil
+				},
+			},
+		},
+	}
+
+	props, err := prop.Export(conn, objectPath, propsSpec)
+	if err != nil {
+		logging.Critical.Panic(err)
+	}
+
+	node := &introspect.Node{
+		Name: objectPath,
+		Interfaces: []introspect.Interface{
+			introspect.IntrospectData,
+			prop.IntrospectData,
+			{
+				Name:       busName,
+				Properties: props.Introspection(busName),
+			},
+		},
+	}
+	err = conn.Export(introspect.NewIntrospectable(node), objectPath, "org.freedesktop.DBus.Introspectable")
+	if err != nil {
+		logging.Critical.Panic(err)
+	}
+
+	logging.Info.Printf("Exposing object %s with interface %s ...", objectPath, busName)
+}
---
+++ system/system.go
@@ -0,0 +1,162 @@
+package system
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/godbus/dbus/v5"
+	"github.com/godbus/dbus/v5/introspect"
+	"github.com/godbus/dbus/v5/prop"
+
+	"github.com/Vioneta/os-agent/udisks2"
+	logging "github.com/Vioneta/os-agent/utils/log"
+)
+
+const (
+	objectPath             = "/io/hass/os/System"
+	ifaceName              = "io.hass.os.System"
+	labelDataFileSystem    = "hassos-data"
+	labelOverlayFileSystem = "hassos-overlay"
+	kernelCommandLine      = "/mnt/boot/cmdline.txt"
+	tmpKernelCommandLine   = "/mnt/boot/.tmp.cmdline.txt"
+	sshAuthKeyFileName     = "/root/.ssh/authorized_keys"
+)
+
+type system struct {
+	conn *dbus.Conn
+}
+
+func getAndCheckBusObjectFromLabel(udisks2helper udisks2.UDisks2Helper, label string) (dbus.BusObject, error) {
+	dataBusObject, err := udisks2helper.GetBusObjectFromLabel(label)
+	if err != nil {
+		return nil, dbus.MakeFailedError(err)
+	}
+
+	dataFilesystem := udisks2.NewFilesystem(dataBusObject)
+	dataMountPoints, err := dataFilesystem.GetMountPointsString(context.Background())
+	if err != nil {
+		return nil, dbus.MakeFailedError(err)
+	}
+
+	if len(dataMountPoints) > 0 {
+		return nil, dbus.MakeFailedError(fmt.Errorf("Device with label \"%s\" is mounted at %s, aborting.", label, dataMountPoints))
+	}
+
+	return dataBusObject, nil
+}
+
+func (d system) WipeDevice() (bool, *dbus.Error) {
+	logging.Info.Printf("Wipe device data.")
+
+	udisks2helper := udisks2.NewUDisks2(d.conn)
+	dataBusObject, err := getAndCheckBusObjectFromLabel(udisks2helper, labelDataFileSystem)
+	if err != nil {
+		return false, dbus.MakeFailedError(err)
+	}
+
+	overlayBusObject, err := getAndCheckBusObjectFromLabel(udisks2helper, labelOverlayFileSystem)
+	if err != nil {
+		return false, dbus.MakeFailedError(err)
+	}
+
+	err = udisks2helper.FormatPartition(dataBusObject, "ext4", labelDataFileSystem)
+	if err != nil {
+		return false, dbus.MakeFailedError(err)
+	}
+	err = udisks2helper.FormatPartition(overlayBusObject, "ext4", labelOverlayFileSystem)
+	if err != nil {
+		return false, dbus.MakeFailedError(err)
+	}
+	logging.Info.Printf("Successfully wiped device data.")
+
+	return true, nil
+}
+
+func (d system) ScheduleWipeDevice() (bool, *dbus.Error) {
+
+	data, err := os.ReadFile(kernelCommandLine)
+	if err != nil {
+		fmt.Println(err)
+		return false, dbus.MakeFailedError(err)
+	}
+
+	datastr := strings.TrimSpace(string(data))
+	datastr += " haos.wipe=1"
+
+	err = os.WriteFile(tmpKernelCommandLine, []byte(datastr), 0644) //nolint:gosec
+	if err != nil {
+		fmt.Println(err)
+		return false, dbus.MakeFailedError(err)
+	}
+
+	// Boot is mounted sync on Home Assistant OS, so just rename should be fine.
+	err = os.Rename(tmpKernelCommandLine, kernelCommandLine)
+	if err != nil {
+		fmt.Println(err)
+		return false, dbus.MakeFailedError(err)
+	}
+
+	logging.Info.Printf("Device will get wiped on next reboot!")
+	return true, nil
+}
+
+func (d system) AddSSHAuthKey(newKey string) *dbus.Error {
+
+	file, err := os.OpenFile(sshAuthKeyFileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		logging.Error.Printf("Failed to open SSH authentication file %s: %s", sshAuthKeyFileName, err)
+		return dbus.MakeFailedError(err)
+	}
+
+	defer file.Close()
+
+	if _, err := file.WriteString(newKey + "\n"); err != nil {
+		logging.Error.Printf("Failed to write SSH authentication file: %s.", err)
+		return dbus.MakeFailedError(err)
+	}
+
+	logging.Info.Printf("New SSH authentication key added for user root.")
+
+	return nil
+}
+
+func (d system) ClearSSHAuthKeys() *dbus.Error {
+	if err := os.Remove(sshAuthKeyFileName); err != nil && os.IsNotExist(err) {
+		logging.Error.Printf("Failed to delete SSH authentication file %s: %s", sshAuthKeyFileName, err)
+		return dbus.MakeFailedError(err)
+	}
+
+	return nil
+}
+
+func InitializeDBus(conn *dbus.Conn) {
+	d := system{
+		conn: conn,
+	}
+
+	err := conn.Export(d, objectPath, ifaceName)
+	if err != nil {
+		logging.Critical.Panic(err)
+	}
+
+	node := &introspect.Node{
+		Name: objectPath,
+		Interfaces: []introspect.Interface{
+			introspect.IntrospectData,
+			prop.IntrospectData,
+			{
+				Name:    ifaceName,
+				Methods: introspect.Methods(d),
+			},
+		},
+	}
+
+	err = conn.Export(introspect.NewIntrospectable(node), objectPath, "org.freedesktop.DBus.Introspectable")
+	if err != nil {
+		logging.Critical.Panic(err)
+	}
+
+	logging.Info.Printf("Exposing object %s with interface %s ...", objectPath, ifaceName)
+}
